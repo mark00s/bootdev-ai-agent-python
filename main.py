c29419e4a5fc4114a42bfc7e364f78ee63a9e124
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-06-20 17:59:35 +0200

    feat: Added args.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+
+if len(sys.argv) < 2:
+    print("No prompt provided. Exiting.")
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 model = "gemini-2.0-flash-001"
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+
+prompt = sys.argv[1]
 
 res = client.models.generate_content(
     model=model,
     contents=prompt,
 )
 
-
 print(res.text)
-
 print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
