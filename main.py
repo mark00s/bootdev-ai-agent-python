97275ebb0dc90d2c478cb6bbe84782a446f00cf9
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-06-20 18:01:52 +0200

    refac: Added main()

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ from dotenv import load_dotenv
 from google import genai
 
 
-if len(sys.argv) < 2:
-    print("No prompt provided. Exiting.")
-    sys.exit(1)
+def main():
+    args = sys.argv[1:]
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    prompt = " ".join(args)
 
-model = "gemini-2.0-flash-001"
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+
+    res = client.models.generate_content(
+        model=model,
+        contents=prompt,
+    )
+
+    print(res.text)
+    print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
 
 
-prompt = sys.argv[1]
-
-res = client.models.generate_content(
-    model=model,
-    contents=prompt,
-)
-
-print(res.text)
-print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
