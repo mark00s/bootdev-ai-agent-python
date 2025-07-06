50cb7d71bb40013da5415cba0bd80d7472bf50b0
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-07-06 17:53:58 +0200

    feat: user_prompt is now stored in messages array.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -12,7 +13,7 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    prompt = " ".join(args)
+    user_prompt = " ".join(args)
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,9 +21,13 @@ def main():
 
     model = "gemini-2.0-flash-001"
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     res = client.models.generate_content(
         model=model,
-        contents=prompt,
+        contents=messages,
     )
 
     print(res.text)
